326889cb765c6d417ac86adf684851c621629ccf
zkboys <876213774@qq.com>
2019-01-31 19:01:47 +0800

    可拖拽tab

--- src/pages/examples/draggable-tabs/index.jsx
+++ src/pages/examples/draggable-tabs/index.jsx
@@ -1,158 +1,36 @@
-import React from 'react';
-import {Tabs} from 'antd';
-import {DragDropContextProvider, DragSource, DropTarget} from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {
+    SortableContainer,
+    SortableElement,
+    arrayMove,
+} from 'react-sortable-hoc';
 
-const TabPane = Tabs.TabPane;
+const SortableItem = SortableElement(({value}) => <li>{value}</li>);
 
-// Drag & Drop node
-class TabNode extends React.Component {
-    render() {
-        const {
-            connectDragSource,
-            connectDropTarget,
-            children,
-        } = this.props
-
-        return connectDragSource(
-            connectDropTarget(children),
-        );
-    }
-}
-
-const cardTarget = {
-    drop(props, monitor) {
-        const dragKey = monitor.getItem().index;
-        const hoverKey = props.index;
-
-        if (dragKey === hoverKey) {
-            return;
-        }
-
-        props.moveTabNode(dragKey, hoverKey);
-        monitor.getItem().index = hoverKey;
-    },
-};
-
-const cardSource = {
-    beginDrag(props) {
-        return {
-            id: props.id,
-            index: props.index,
-        }
-    },
-};
-
-const WrapTabNode = DropTarget(
-    'DND_NODE',
-    cardTarget,
-    (connect) => ({
-        connectDropTarget: connect.dropTarget(),
-    }),
-)(
-    DragSource(
-        'DND_NODE',
-        cardSource,
-        (connect, monitor) => ({
-            connectDragSource: connect.dragSource(),
-            isDragging: monitor.isDragging(),
-        }),
-    )(TabNode),
-);
-
-class DraggableTabs extends React.Component {
-    state = {
-        order: [],
-    };
-
-    moveTabNode = (dragKey, hoverKey) => {
-        const newOrder = this.state.order.slice();
-        const {children} = this.props;
-
-        React.Children.forEach(children, (c) => {
-            if (newOrder.indexOf(c.key) === -1) {
-                newOrder.push(c.key);
-            }
-        });
-
-        const dragIndex = newOrder.indexOf(dragKey);
-        const hoverIndex = newOrder.indexOf(hoverKey);
-
-        newOrder.splice(dragIndex, 1);
-        newOrder.splice(hoverIndex, 0, dragKey);
-
-        this.setState({
-            order: newOrder,
-        });
-    };
-
-    renderTabBar = (props, DefaultTabBar) => (
-        <DefaultTabBar {...props}>
-            {node => (
-                <WrapTabNode key={node.key} index={node.key} moveTabNode={this.moveTabNode}>{node}</WrapTabNode>
-            )}
-        </DefaultTabBar>
+const SortableList = SortableContainer(({items}) => {
+    return (
+        <ul>
+            {items.map((value, index) => (
+                <SortableItem key={`item-${index}`} index={index} value={value} />
+            ))}
+        </ul>
     );
+});
 
+class SortableComponent extends Component {
+    state = {
+        items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+    };
+    onSortEnd = ({oldIndex, newIndex}) => {
+        this.setState(({items}) => ({
+            items: arrayMove(items, oldIndex, newIndex),
+        }));
+    };
     render() {
-        const {order} = this.state;
-        const {children} = this.props;
-
-        const tabs = [];
-        React.Children.forEach(children, (c) => {
-            tabs.push(c);
-        });
-
-        const orderTabs = tabs.slice().sort((a, b) => {
-            const orderA = order.indexOf(a.key);
-            const orderB = order.indexOf(b.key);
-
-            if (orderA !== -1 && orderB !== -1) {
-                return orderA - orderB;
-            }
-            if (orderA !== -1) {
-                return -1;
-            }
-            if (orderB !== -1) {
-                return 1;
-            }
-
-            const ia = tabs.indexOf(a);
-            const ib = tabs.indexOf(b);
-
-            return ia - ib;
-        });
-
-        return (
-            <DragDropContextProvider backend={HTML5Backend}>
-                <Tabs
-                    renderTabBar={this.renderTabBar}
-                    {...this.props}
-                >
-                    {orderTabs}
-                </Tabs>
-            </DragDropContextProvider>
-        );
+        return <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />;
     }
 }
+export const PAGE_ROUTE = '/draggable-tabs';
 
-export const PAGE_ROUTE = '/example/draggable-tabs';
-
-export default class DraggableTabsComponent extends React.Component {
-
-    render() {
-        return (
-            <DraggableTabs>
-                <TabPane tab="tab 1" key="1">
-                    Content of Tab Pane 1
-                </TabPane>
-                <TabPane tab="tab 2" key="2">
-                    Content of Tab Pane 2
-                </TabPane>
-                <TabPane tab="tab 3" key="3">
-                    Content of Tab Pane 3
-                </TabPane>
-            </DraggableTabs>
-        );
-    }
-}
+export default SortableComponent;
