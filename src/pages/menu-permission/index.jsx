f981f73ad0c4d2711e59bf58ad0a50c24b4beca9
zkboys <876213774@qq.com>
2019-02-12 17:45:10 +0800

    菜单&权限

--- src/pages/menu-permission/index.jsx
+++ src/pages/menu-permission/index.jsx
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import {Table, Icon, Tooltip} from 'antd';
+import {Table, Icon, Tooltip, Modal, Form, Row, Col} from 'antd';
 import config from '@/commons/config-hoc';
 import PageContent from '@/layouts/page-content';
 import localMenus from '../../menus';
 import {convertToTree} from "@/library/utils/tree-utils";
-import {ToolBar, Operator} from '@/library/antd';
+import {ToolBar, Operator, FormElement} from '@/library/antd';
 import './style.less';
 
 @config({
@@ -12,24 +12,14 @@ import './style.less';
     title: {local: 'menus', text: '菜单&权限'},
     ajax: true,
 })
+@Form.create()
 export default class index extends Component {
     state = {
         menus: [],
+        visible: false,
+        record: {},
     };
 
-    /*
-    * key|是|需要唯一
-parentKey|否|用于关联父级
-path|是|菜单对应的路由地址
-text|是|如果local对应的文案有效，将覆盖text，否则菜单默认使用text作为展示内容
-icon|否|菜单图标配置
-url|否|菜单对应会打开url对应的iframe页面，如果配置了url，path将无效
-target|否|配合url使用，菜单将为a标签 `<a href={url} target={target}>{text}</a>`
-local|否|国际化配置，系统在i18n.menu中获取对应的文案
-order|否|菜单排序，数值越大越靠前显示
-type|否|如果菜单数据中携带功能权限配置，type==='1' 为菜单，type==='2'为功能
-code|否|功能码，如果是type==='2'，会用到此字段
-    * */
     columns = [
         // key 与parentKey自动生成了，不需要展示和编辑
         // {title: 'key', dataIndex: 'key', key: 'key'},
@@ -46,8 +36,8 @@ code|否|功能码，如果是type==='2'，会用到此字段
         },
         {title: '图标', dataIndex: 'icon', key: 'icon', width: 60, render: value => <Icon type={value}/>},
         {title: 'path', dataIndex: 'path', key: 'path', width: 100},
-        // {title: 'url', dataIndex: 'url', key: 'url'},
-        // {title: 'target', dataIndex: 'target', key: 'target', width: 60},
+        {title: 'url', dataIndex: 'url', key: 'url'},
+        {title: 'target', dataIndex: 'target', key: 'target', width: 60},
         {title: '国际化', dataIndex: 'local', key: 'local', width: 60},
         {
             title: '类型', dataIndex: 'type', key: 'type', width: 60,
@@ -130,28 +120,78 @@ code|否|功能码，如果是type==='2'，会用到此字段
     }
 
     handleAddTopMenu = () => {
-        // TODO 弹框添加
-        console.log('添加顶级');
+        this.props.form.resetFields();
+        this.setState({visible: true});
     };
 
     handleEditNode = (record) => {
-        console.log(record);
+        const {resetFields, setFieldsValue} = this.props.form;
+
+        resetFields();
+        const {
+            text,
+            icon,
+            path,
+            url,
+            target,
+            local,
+            type = '1',
+            code,
+            order,
+        } = record;
+
+        setTimeout(() => {
+            setFieldsValue({
+                text,
+                icon,
+                path,
+                url,
+                target,
+                local,
+                type,
+                code,
+                order,
+            })
+        });
+        this.setState({visible: true, record});
     };
 
     handleAddSubMenu = (record) => {
-        console.log(record);
+        this.props.form.resetFields();
+        this.setState({visible: true, record});
     };
 
     handleAddSubFunction = (record) => {
-        console.log(record);
+        const {resetFields, setFieldsValue} = this.props.form;
+
+        resetFields();
+        setFieldsValue({type: '2'});
+
+        this.setState({visible: true, record});
     };
 
     handleDeleteNode = (record) => {
         console.log(record);
     };
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
     render() {
-        const {menus} = this.state;
+        const {
+            menus,
+            visible,
+        } = this.state;
+
+        const {form} = this.props;
+
+        const labelWidth = 70;
 
         return (
             <PageContent styleName="root">
@@ -160,6 +200,123 @@ code|否|功能码，如果是type==='2'，会用到此字段
                     columns={this.columns}
                     dataSource={menus}
                 />
+                <Modal
+                    id="menu-modal"
+                    title="菜单&权限"
+                    visible={visible}
+                    onOk={this.handleSubmit}
+                    onCancel={() => this.setState({visible: false})}
+                >
+                    <Form onSubmit={this.handleSubmit}>
+                        <Row>
+                            <Col span={12}>
+                                <FormElement
+                                    form={form}
+                                    label="名称"
+                                    labelWidth={labelWidth}
+                                    type="input"
+                                    field="text"
+                                    placeholder="请输入名称"
+                                    decorator={{
+                                        rules: [
+                                            {required: true, message: '请输入名称！'},
+                                        ],
+                                    }}
+                                />
+                            </Col>
+                            <Col span={12}>
+                                <FormElement
+                                    form={form}
+                                    label="图标"
+                                    labelWidth={labelWidth}
+                                    type="input"
+                                    field="icon"
+                                    placeholder="请输入图标"
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col span={12}>
+                                <FormElement
+                                    form={form}
+                                    label="类型"
+                                    labelWidth={labelWidth}
+                                    type="select"
+                                    options={[
+                                        {value: '1', label: '菜单'},
+                                        {value: '2', label: '功能'},
+                                    ]}
+                                    field="type"
+                                    placeholder="请选择类型"
+                                    decorator={{initialValue: '1'}}
+                                    getPopupContainer={() => document.querySelector('.ant-modal-wrap')}
+                                />
+                            </Col>
+                            <Col span={12}>
+                                <FormElement
+                                    disabled={form.getFieldValue('type') !== '2'}
+                                    form={form}
+                                    label="编码"
+                                    labelWidth={labelWidth}
+                                    type="input"
+                                    field="code"
+                                    placeholder="请输入功能编码"
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col span={12}>
+                                <FormElement
+                                    form={form}
+                                    label="国际化"
+                                    labelWidth={labelWidth}
+                                    type="input"
+                                    field="local"
+                                    placeholder="请输入国际化"
+                                />
+                            </Col>
+                            <Col span={12}>
+                                <FormElement
+                                    form={form}
+                                    label="排序"
+                                    labelWidth={labelWidth}
+                                    type="number"
+                                    field="order"
+                                    placeholder="请输入排序"
+                                    min={0}
+                                    step={1}
+                                />
+                            </Col>
+                        </Row>
+                        <FormElement
+                            disabled={form.getFieldValue('type') === '2'}
+                            form={form}
+                            label="path"
+                            labelWidth={labelWidth}
+                            type="input"
+                            field="path"
+                            placeholder="请输入path"
+                        />
+                        <FormElement
+                            disabled={form.getFieldValue('type') === '2'}
+                            form={form}
+                            label="url"
+                            labelWidth={labelWidth}
+                            type="input"
+                            field="url"
+                            placeholder="请输入url"
+                        />
+                        <FormElement
+                            disabled={form.getFieldValue('type') === '2'}
+                            form={form}
+                            label="target"
+                            labelWidth={labelWidth}
+                            type="input"
+                            field="target"
+                            placeholder="请输入target"
+                        />
+                    </Form>
+                </Modal>
             </PageContent>
         );
     }
